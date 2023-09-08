9122bb70959bd2f52244286b071fbb009b3752f7
7R1XY <theo.broden@gmail.com>
2023-09-08 13:30:20 +0200

    Add project files.

---
+++ Övning 2.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Vilken stad kommer du ifrån?");
+            string stad = Console.ReadLine();
+            Console.WriteLine("Vad är ditt drömresemål?");
+            string resemål = Console.ReadLine();
+
+            Console.WriteLine("Hej " + stad + "bo. Jag hoppas att du får resa till " + resemål + ".");
+            
+            Console.ReadKey();
+        }
+    }
+}
